412cc35cd8f6f4ef8992fd23993e21672afd8cc6
Tiago Almeida <tiago.almeida@bluefinsolutions.com>
2016-10-13 19:46:45 +0100

    Started adding support for $order_by

--- django_odata/views.py
+++ django_odata/views.py
@@ -54,14 +54,54 @@ class OdataJsonSerializer(Serializer):
 		return super(Serializer, self).getvalue()
 
 
+def _set_order_by(orm_queryset, odata_orderby):
+	"""
+	Takes a django ORM query and applies an ordering constraint
+	if the odata_orderby contains a valid $order_by odata get param.
+	Below you can find some examples of what it needs to handle.
+	The ordering is done by supplying the proper parameters
+	to the .order_by method of the QuerySet
+	Order by examples:
+	------------------
+	http://example.com/OData/OData.svc/Products?$orderby=Rating
+		All Product Entries returned in ascending order when sorted
+		by the Rating Property.
+	
+	http://example.com/OData/OData.svc/Products?$orderby=Rating asc
+		Same as the example above.
+	
+	http://example.com/OData/OData.svc/Products?$orderby=...
+		...Rating,Category/Name desc
+		Same as the URI above except the set of Products is subsequently
+		 sorted (in descending order) by the Name property of the
+		 related Category Entry.
+	"""
+	result = orm_queryset
+	if odata_orderby:
+		django_order_tuple = tuple()
+		# Break the odata query by ,
+		for criteria in odata_orderby.split(',')
+			django_order_tuple += (criteria,) 
+		return result.order_by(*django_order_tuple)
+	return result
+
+
 def get_set(request, set_name):
+	"""
+	Returns a set of objects.
+	For help on what this should comply with, read:
+	http://www.odata.org/documentation/odata-version-2-0/uri-conventions/
+	
+	
+
+	"""
+	# System Query Options
+	q_order_by = request.GET.get('$order_by', False)
 	EntityModel = apps.get_model('webapp', set_name)
 	query_result = EntityModel.objects.all()
-	response_obj = {
-		"d": {
-			"results": query_result
-		}
-	}
+	if q_order_by:
+		query_result = _order_by(query_result, q_order_by)
+		
 	service_root = reverse('odata_service_root')
 	s = OdataJsonSerializer(service_root, set_name)
 	#return HttpResponse(s.serialize(response_obj))
