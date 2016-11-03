13ce0d266738ac8ea864b51dda8fabd436a095c6
Tiago Almeida <tiago.almeida@bluefinsolutions.com>
2016-11-03 20:50:18 +0000

    Filter parser with ply

--- django_odata/filterparser.py
+++ django_odata/filterparser.py
@@ -1,4 +1,4 @@
-# ============================================================
+﻿# ============================================================
 # filterparser.py
 #
 # (C) Tiago Almeida 2016
@@ -9,11 +9,14 @@
 
 import ply.lex as lex
 import ply.yacc as yacc
+import unittest
 
 # ============================================================
 # Lexer rules
 # ============================================================
-reserved = {}
+reserved = {
+   #'and': 'AND'
+}
 
 
 # List of token names.
@@ -35,19 +38,21 @@ tokens = (
 
 
 # Regular expression rules for simple tokens
-t_EQ			= r'eq'
+t_EQ			= 'eq'
 t_NE			= r'ne'
 t_GT			= r'gt'
 t_GE			= r'gt'
 t_LT			= r'lt'
 t_LE 			= r'le'
-t_AND 		= r'and'
-t_OR 			= r'or'
 t_LPAREN 	= r'\('
 t_RPAREN 	= r'\)'
 t_ID			= r'[a-zA-Z_][a-zA-Z0-9_]*'
-t_ignore 	= ' \t' # ignored characters (spaces and tabs)
+t_ignore = " \t" # ignored characters (spaces and tabs)
 t_STARTSWITH = r'startswith'
+
+def t_OR(t):
+	'or'
+	return {}
 
 
 def t_NUMBER(t):
@@ -56,22 +61,87 @@ def t_NUMBER(t):
 	return t
 
 
+def t_error(t):
+	print("Illegal character '%s'" % t.value[0])
+	t.lexer.skip(1)
+
+
+lexer = lex.lex()
+
 # ============================================================
 # Parser rules
 # ============================================================
-def p_filter_def(t):
-	'''filter : constraint
-						| constraint OR constraint
-						| constraint AND constraint
-	'''
-	pass
+precedence = (
+	('left','OR'),
+	('left','AND')
+)
+
+
+def debug_print(str):
+	print(str)
+
+
+def p_filter(t):
+	'filter : constraint'
+	debug_print('filter')
+	t[0] = t[1]
 
 
 def p_constraint(t):
-	'constraint : ID OPERATOR VALUE'
-	pass
+	'constraint : ID NUMBER'
+	debug_print('p_constraint')
+	t[0] = {}
 
 
 def p_constraint_group(t):
 	'constraint : LPAREN constraint RPAREN'
-	pass
+	debug_print('parenthesis')
+	t[0] = {}
+
+
+def p_constraint_binop(t):
+	'''constraint : constraint OR constraint
+								| constraint AND constraint
+	'''
+	t[0] = {}
+
+
+
+def p_operator(t):
+	"""
+	operator 	: EQ
+						| GT
+						| GE
+						| LT
+						| LE
+						| NE
+	"""
+	t[0] = {}
+
+
+
+def p_error(t):
+	# TODO 
+	print("Syntax error at '%s'" % t.value)
+
+
+parser = yacc.yacc()
+
+def parse(filter_expression):
+	return parser.parse(filter_expression)
+
+
+
+
+# ============================================================
+# Unit tests
+# ============================================================
+
+class TestCase(unittest.TestCase):
+	def test1(self):
+		parse('teste 1 or test 3')
+		#self.assertEqual((1 + 2), 3)
+		#self.assertEqual(0 + 1, 1)
+
+if __name__ == '__main__':
+	unittest.main()
